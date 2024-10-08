5cb7a28cb7ed60538a01dd23d6be21246bf702ab
Deepak mane <deepakmane33@gmail.com>
2024-10-08 13:43:11 +0530

    Create HashMapBasics.java

---
+++ Collections/HashMapBasics.java
@@ -0,0 +1,42 @@
+package arraylist;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class HashMapBasics {
+
+	public static void main(String[] args) {
+		
+		 Map<Integer,String> map = new HashMap<>();
+		 
+		 map.put(1, "Deep");
+		 map.put(2,"Sam");
+		 map.put(3, "John");
+		 map.put(4, "Ritu");
+		 
+		 
+		 Iterator<Integer> itr = map.keySet().iterator();
+		 
+		 while(itr.hasNext()) {
+			 				Integer k = itr.next();
+			 				
+			 				String v = map.get(k);
+			 				
+			 				System.out.println("key= "+ k +" value= "+ v);
+		 }
+		 
+		 System.out.println("*****************");
+		 
+		  Iterator<Entry<Integer, String>> itr2 = map.entrySet().iterator();
+		  
+		  while(itr2.hasNext()) {
+			  
+			     Entry<Integer, String> next = itr2.next();
+			     
+			     System.out.println("k="+next.getKey() + " v= "+ next.getValue());
+		  }
+		
+
+	}
+
+}
